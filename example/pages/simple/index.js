79d3eb87b75e63c4706efa140d9ca612f6f8119a
枭白 <crystal.cj@alibaba-inc.com>
2019-05-14 11:07:25 +0800

    fix: 兼容行问题，改为fixed

--- example/pages/simple/index.js
+++ example/pages/simple/index.js
@@ -14,26 +14,18 @@ class Simple extends React.Component {
 
 
   render() {
-    const containerStyle = {
-      width: '15rem',
-      height: '30rem',
-      background: '#04AFD2',
-      position: 'relative',
-    };
     const sidebarStyle = {
       height: '7.77rem',
     };
     const sidebar = <div>test</div>;
     return (<div>
       <button type="button" onClick={this.changeDrawer}>Click Me!</button>
-      <div style={containerStyle}>
-        <BizDrawer
-          isVisible={this.state.isVisible}
-          sidebar={sidebar}
-          sidebarStyle={sidebarStyle}
-          onChangeVisible={this.changeDrawer}
-        />
-      </div>
+      <BizDrawer
+        isVisible={this.state.isVisible}
+        sidebar={sidebar}
+        sidebarStyle={sidebarStyle}
+        onChangeVisible={this.changeDrawer}
+      />
     </div>)
   }
 }
